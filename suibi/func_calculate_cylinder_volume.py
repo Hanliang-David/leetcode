ecfc37a9346114fb079e024e63d57c991be062d8
Hanliang-David <48267997+Hanliang-David@users.noreply.github.com>
2024-07-16 16:50:59 +0800

    Create func_calculate_cylinder_volume.py

---
+++ suibi/func_calculate_cylinder_volume.py
@@ -0,0 +1,7 @@
+# func_calculate_cylinder_volume.py
+import math
+
+def func_calculate_cylinder_volume(radius, height):
+    return math.pi * radius ** 2 * height
+
+print(func_calculate_cylinder_volume(3, 5))
