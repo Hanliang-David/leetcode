08cc19c770f01e33d667eff010615f19f3a0bbca
Hanliang-David <48267997+Hanliang-David@users.noreply.github.com>
2024-07-19 23:25:24 +0800

    Create func_calculate_perimeter_of_rectangle.py

---
+++ suibi/func_calculate_perimeter_of_rectangle.py
@@ -0,0 +1,5 @@
+# func_calculate_perimeter_of_rectangle.py
+def func_calculate_perimeter_of_rectangle(length, width):
+    return 2 * (length + width)
+
+print(func_calculate_perimeter_of_rectangle(5, 3))
