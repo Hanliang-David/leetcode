707139fdd2bde6391b50f73401fb924e0834f2f4
Hanliang-David <48267997+Hanliang-David@users.noreply.github.com>
2024-07-24 11:19:19 +0800

    Create fun_py_calculate_circle_circumference.py

---
+++ suibi/fun_py_calculate_circle_circumference.py
@@ -0,0 +1,7 @@
+# fun_py_calculate_circle_circumference.py
+import math
+
+def fun_py_calculate_circle_circumference(radius):
+    return 2 * math.pi * radius
+
+print(fun_py_calculate_circle_circumference(7))
