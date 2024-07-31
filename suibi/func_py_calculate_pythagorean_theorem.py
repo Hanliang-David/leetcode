1a93ac6a5d884430962c588aa0fe1838f3ff0372
Hanliang-David <48267997+Hanliang-David@users.noreply.github.com>
2024-07-31 18:04:39 +0800

    Create func_py_calculate_pythagorean_theorem.py

---
+++ suibi/func_py_calculate_pythagorean_theorem.py
@@ -0,0 +1,7 @@
+# func_py_calculate_pythagorean_theorem.py
+import math
+
+def func_py_calculate_pythagorean_theorem(a, b):
+    return math.sqrt(a**2 + b**2)
+
+print(func_py_calculate_pythagorean_theorem(3, 4))
