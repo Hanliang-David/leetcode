ab265359ab810e44a31d102098d729f5d3a568cf
Hanliang-David <48267997+Hanliang-David@users.noreply.github.com>
2024-08-15 21:02:20 +0800

    Create func_py_calculate_parallelogram_perimeter.py

---
+++ suibi/func_py_calculate_parallelogram_perimeter.py
@@ -0,0 +1,5 @@
+# func_py_calculate_parallelogram_perimeter.py
+def func_py_calculate_parallelogram_perimeter(base, side):
+    return 2 * (base + side)
+
+print(func_py_calculate_parallelogram_perimeter(5, 7))
