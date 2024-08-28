37462f0c726c873c3bcd4b4c2da005ff58e2854e
Hanliang-David <48267997+Hanliang-David@users.noreply.github.com>
2024-08-28 20:36:20 +0800

    Create func_py_calculate_kepler_triangle_area.py

---
+++ suibi/func_py_calculate_kepler_triangle_area.py
@@ -0,0 +1,7 @@
+# func_py_calculate_kepler_triangle_area.py
+import math
+
+def func_py_calculate_kepler_triangle_area(a):
+    return a * (a * math.sqrt(2)) / 2
+
+print(func_py_calculate_kepler_triangle_area(5))
